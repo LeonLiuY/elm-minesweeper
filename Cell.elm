4b6c66546674c7aa8df66240a75c6e702cb1c585
Liu Yang <liu_yang@worksap.co.jp>
2016-09-20 15:13:29 +0900

    Format.

--- Cell.elm
+++ Cell.elm
@@ -1,8 +1,10 @@
 module Cell exposing (Msg, Model, update, view, CellValue(..), CellStatus(..))
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Json.Decode exposing (succeed)
+
 
 type CellValue
     = Mine
@@ -20,32 +22,57 @@ type alias Cell =
     , status : CellStatus
     }
 
-type alias Model = Cell
 
-type Msg = Open | ToggleMark | NoOp
+type alias Model =
+    Cell
 
-update: Msg -> Model -> Model
 
+type Msg
+    = Open
+    | ToggleMark
+    | NoOp
+
+
+update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Open -> {model | status = Opened}
-    ToggleMark -> {model | status = if model.status == Marked then Covered else Marked}
-    NoOp -> model
+    case msg of
+        Open ->
+            { model | status = Opened }
+
+        ToggleMark ->
+            { model
+                | status =
+                    if model.status == Marked then
+                        Covered
+                    else
+                        Marked
+            }
+
+        NoOp ->
+            model
 
 
 styles =
-  let size = "24px"
-      common' = [("width", size), ("height", size)]
-  in
-  {
-    common = common',
-    marked = ("background", "red") :: common'
-  }
+    let
+        size =
+            "24px"
 
-view: Model -> Html Msg
+        common' =
+            [ ( "width", size ), ( "height", size ) ]
+    in
+        { common = common'
+        , marked = ( "background", "red" ) :: common'
+        }
 
+
+view : Model -> Html Msg
 view model =
-  case model.status of
-    Covered -> td [style styles.common, onClick Open, (onWithOptions "contextmenu" {defaultOptions | preventDefault = True} (succeed ToggleMark))] []
-    Opened -> td [style styles.common, (onWithOptions "contextmenu" {defaultOptions | preventDefault = True} (succeed NoOp))] []
-    Marked -> td [style styles.marked, (onWithOptions "contextmenu" {defaultOptions | preventDefault = True} (succeed ToggleMark))] []
+    case model.status of
+        Covered ->
+            td [ style styles.common, onClick Open, (onWithOptions "contextmenu" { defaultOptions | preventDefault = True } (succeed ToggleMark)) ] []
+
+        Opened ->
+            td [ style styles.common, (onWithOptions "contextmenu" { defaultOptions | preventDefault = True } (succeed NoOp)) ] []
+
+        Marked ->
+            td [ style styles.marked, (onWithOptions "contextmenu" { defaultOptions | preventDefault = True } (succeed ToggleMark)) ] []
--- Main.elm
+++ Main.elm
@@ -9,25 +9,30 @@ main =
     App.beginnerProgram { model = init, view = view, update = update }
 
 
+
 -- MODEL
 
-type alias ID= Int
 
-type alias Model ={
-    cells: List (ID, Cell.Model),
-    nextID : ID
-  }
+type alias ID =
+    Int
 
 
-init: Model
+type alias Model =
+    { cells : List ( ID, Cell.Model )
+    , nextID : ID
+    }
 
-init = {
-  cells = [(0, {value = Mine, status = Marked}),
-  (1, {value = Mine, status = Covered}),
-  (2, {value = Mine, status = Opened}),
-  (3, {value = Mine, status = Marked})],
-  nextID = 4
-  }
+
+init : Model
+init =
+    { cells =
+        [ ( 0, { value = Mine, status = Marked } )
+        , ( 1, { value = Mine, status = Covered } )
+        , ( 2, { value = Mine, status = Opened } )
+        , ( 3, { value = Mine, status = Marked } )
+        ]
+    , nextID = 4
+    }
 
 
 
@@ -40,15 +45,16 @@ type Msg
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Action id cellMsg ->
-    let updateCell (cellID, cellModel) =
-      if cellID == id then
-        (cellID, Cell.update cellMsg cellModel)
-        else
-          (cellID, cellModel)
-          in
-      {model | cells = List.map updateCell model.cells}
+    case msg of
+        Action id cellMsg ->
+            let
+                updateCell ( cellID, cellModel ) =
+                    if cellID == id then
+                        ( cellID, Cell.update cellMsg cellModel )
+                    else
+                        ( cellID, cellModel )
+            in
+                { model | cells = List.map updateCell model.cells }
 
 
 
@@ -57,10 +63,12 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  div
-  []
-    <| List.map viewCell model.cells
+    div
+        []
+    <|
+        List.map viewCell model.cells
+
 
 viewCell : ( ID, Cell.Model ) -> Html Msg
-viewCell (id, model) =
-  App.map (Action id) (Cell.view model)
+viewCell ( id, model ) =
+    App.map (Action id) (Cell.view model)
